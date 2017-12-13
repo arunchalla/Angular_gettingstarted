3b0899e8c43e49f0f2913353d6cdfba5bcc34f20
arunchalla <challaarunkr@gmail.com>
2017-12-12 19:48:25 -0600

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -8,6 +9,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component'
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
@@ -16,14 +18,23 @@ import { ProductDetailComponent } from './products/product-detail.component'
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
     
   ],
 
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path:'products',component:ProductListComponent},
+      {path :'products/:id',component:ProductDetailComponent},
+      {path :'welcome',component:WelcomeComponent},
+      {path :'',redirectTo:'welcome', pathMatch: 'full'},
+      {path :'**',redirectTo:'welcome', pathMatch: 'full'}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent] 
